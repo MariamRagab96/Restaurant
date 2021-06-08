cdd18695b031a497dcc3788052644f9123d130cc
MariamRagab96 <mariamragab.ieee@gmail.com>
2021-06-07 23:56:44 -0700

    The View Of Login and Registration With Routing between them

--- restaurant-front/src/app/app-routing.module.ts
+++ restaurant-front/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RegistrationComponent } from './components/registration/registration.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "login"  , component: LoginComponent},
+  {path: "registration",component :RegistrationComponent},
+  {path: ''  , component: LoginComponent}
+
+];
 
 @NgModule({
+  // forRoot(routes) -->  the name of above array
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
