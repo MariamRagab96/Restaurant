66dcc09280da76622dbf70831f734fb1f0ef1cac
MariamRagab96 <mariamragab.ieee@gmail.com>
2021-06-13 12:28:57 -0700

    create user table on mySql workbench and connect on database then adding users in it using postman

---
+++ restaurant-backend/src/main/java/com/spring/code/model/User.java
@@ -0,0 +1,42 @@
+package com.spring.code.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
+@RequiredArgsConstructor
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @Column(name = "user_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "user_first_name")
+    private String firstName;
+
+    @Column(name = "user_last_name")
+    private String lastName;
+
+    @Column(name = "user_mail")
+    private String mail;
+
+    @Column(name = "user_phone")
+    private String phone;
+
+    @Column(name = "user_password")
+    private String password;
+
+}
---
+++ restaurant-backend/src/main/java/com/spring/code/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.spring.code.repository;
+
+import com.spring.code.model.User;
+import java.util.Optional;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+
+}
