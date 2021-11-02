89d66a0e6684be1fa1af8be511370dea4e7c8a13
MariamRagab96 <mariamragab.ieee@gmail.com>
2021-11-02 23:06:11 +0200

    adding product,category and user repositories

---
+++ restaurant-backend/src/main/java/com/spring/repository/CategoryRepository.java
@@ -0,0 +1,17 @@
+
+package com.spring.repository;
+
+import com.spring.model.Category;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+
+     @Query("SELECT c FROM Category c")
+    List<Category> getAll();
+    
+}
